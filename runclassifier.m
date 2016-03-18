4aa2d5bbd5ee599d68b0308f709ad5837841e08d
Srivas Chennu <srivas@gmail.com>
2016-03-18 00:00:01 +0000

    Checked in Fri Mar 18 00:00:01 GMT 2016
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Liege-RestingState@7 1986c888-7f2f-4208-b3bb-bcc9d8df033b

--- runclassifier.m
+++ runclassifier.m
@@ -6,31 +6,39 @@ param = finputcheck(varargin, {
     });
 loadsubj
 
+bands = {
+    'delta'
+    'theta'
+    'alpha'
+    };
+
 featlist = {
-    'ftdwpli','power',1
-    'ftdwpli','power',2
-    'ftdwpli','power',3
+%     'ftdwpli','power',1
+%     'ftdwpli','power',2
+%     'ftdwpli','power',3
     'ftdwpli','median',1
     'ftdwpli','median',2
     'ftdwpli','median',3
-    'ftdwpli','participation coefficient',1
-    'ftdwpli','participation coefficient',2
-    'ftdwpli','participation coefficient',3
-    'ftdwpli','small-worldness',1
-    'ftdwpli','small-worldness',2
-    'ftdwpli','small-worldness',3
-    'ftdwpli','centrality',1
-    'ftdwpli','centrality',2
-    'ftdwpli','centrality',3
-    'ftdwpli','modular span',1
-    'ftdwpli','modular span',2
-    'ftdwpli','modular span',3
+%     'ftdwpli','participation coefficient',1
+%     'ftdwpli','participation coefficient',2
+%     'ftdwpli','participation coefficient',3
+%     'ftdwpli','small-worldness',1
+%     'ftdwpli','small-worldness',2
+%     'ftdwpli','small-worldness',3
+%     'ftdwpli','centrality',1
+%     'ftdwpli','centrality',2
+%     'ftdwpli','centrality',3
+%     'ftdwpli','modular span',1
+%     'ftdwpli','modular span',2
+%     'ftdwpli','modular span',3
     };
 subjlist = eval(listname);
 
 features = zeros(size(subjlist,1),size(featlist,1));
+featnames = cell(1,size(featlist,1));
 for f = 1:size(featlist,1)
     [features(:,f),groupvar] = plotmeasure(listname,featlist{f,:},'noplot','on',varargin{:});
+    featnames{f} = strrep(strrep(sprintf('%s_%s',featlist{f,2},bands{featlist{f,3}}),' ','_'),'-','_');
 end
 
 groups = unique(groupvar(~isnan(groupvar)));
@@ -38,8 +46,10 @@ grouppairs = nchoosek(groups,2);
 
 fprintf('\n');
 for g = 1:size(grouppairs,1)
-    svmmodel = fitcsvm(features(groupvar == grouppairs(g,1) | groupvar == grouppairs(g,2),:),...
-        groupvar(groupvar == grouppairs(g,1) | groupvar == grouppairs(g,2)),...
+    feattable = array2table(features(groupvar == grouppairs(g,1) | groupvar == grouppairs(g,2),:),'VariableNames',featnames);
+    grouptable = array2table(groupvar(groupvar == grouppairs(g,1) | groupvar == grouppairs(g,2)),'VariableNames',{'group'});
+    datatable = horzcat(feattable,grouptable);
+    svmmodel = fitcsvm(datatable,'group',...
         'LeaveOut','on',...
         'KernelScale','auto','Standardize',true,'KernelFunction','RBF');
     fprintf('%s vs. %s accuracy = %.2f%%.\n',param.groupnames{grouppairs(g,1)+1},...
