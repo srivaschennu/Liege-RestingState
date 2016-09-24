4630b0ee74711c443066d745c47a4420b8d4b55b
Srivas Chennu <srivas@gmail.com>
2016-09-24 23:00:01 +0000

    Checked in Sun Sep 25 00:00:01 BST 2016
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Liege-RestingState@51 1986c888-7f2f-4208-b3bb-bcc9d8df033b

--- testclassifier.m
+++ testclassifier.m
@@ -23,6 +23,8 @@ load(sprintf('clsyfyr_%s.mat',param.group),'clsyfyr','featlist');
 
 subjlist = eval(listname);
 refdiag = cell2mat(subjlist(:,2));
+refaware = double(cell2mat(subjlist(:,2)) > 0);
+refaware(isnan(refdiag)) = NaN;
 crsdiag = cell2mat(subjlist(:,3));
 crsaware = double(cell2mat(subjlist(:,3)) > 0);
 petdiag = cell2mat(subjlist(:,4));
@@ -34,6 +36,12 @@ outcome(isnan(cell2mat(subjlist(:,10)))) = NaN;
 mcstennis = tennis .* crsdiag;
 mcstennis(crsdiag == 0) = NaN;
 crs = cell2mat(subjlist(:,11));
+
+admvscrs = NaN(size(refdiag));
+admvscrs(refaware == 0) = 0;
+admvscrs(refaware == 0 & crsaware == 0) = 0;
+admvscrs(refaware > 0 & crsaware > 0) = 1;
+admvscrs(refaware == 0 & crsaware > 0) = 2;
 
 groupvar = eval(param.group);
 
