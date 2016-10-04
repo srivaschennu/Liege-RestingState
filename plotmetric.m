93804472cb42d8b38ac18aa0fce07b1e369152f5
Srivas Chennu <srivas@gmail.com>
2016-10-04 23:00:02 +0000

    Checked in Wed Oct  5 00:00:02 BST 2016
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Liege-RestingState@55 1986c888-7f2f-4208-b3bb-bcc9d8df033b

--- plotmetric.m
+++ plotmetric.m
@@ -18,13 +18,13 @@ param = finputcheck(varargin, {
 fontname = 'Helvetica';
 fontsize = 28;
 
-% load(sprintf('%s/%s/%s%sgraph.mat',filepath,conntype,basename,conntype));
-load(sprintf('%s/%s/%s%sfdr.mat',filepath,conntype,basename,conntype));
+load(sprintf('%s/%s/%s%sgraph.mat',filepath,conntype,basename,conntype));
+load(sprintf('%s/%s/%s%s.mat',filepath,conntype,basename,conntype));
 
-% load(sprintf('%s/%s/graphdata_%s_%s.mat',filepath,conntype,listname,conntype));
+load(sprintf('%s/%s/graphdata_%s_%s.mat',filepath,conntype,listname,conntype));
 load(sprintf('%s/%s/alldata_%s_%s.mat',filepath,conntype,listname,conntype));
 
-weiorbin = 2;
+weiorbin = 3;
 
 if strcmpi(measure,'small-worldness') || strcmp(param.randratio,'on')
     if exist(sprintf('%s/%s/%s%srandgraph.mat',filepath,conntype,basename,conntype),'file')
@@ -51,23 +51,12 @@ elseif strcmp(param.randratio,'on')
     graph{m,2} = graph{m,2} ./ mean(randgraph.graph{m,2},ndims(randgraph.graph{m,2}));
 end
 
-v1idx = zeros(size(subjlist,1),1);
-for s = 1:size(subjlist,1)
-    if ~isempty(subjlist{s,5})
-        v1idx(s) = find(strcmp(subjlist{s,5},subjlist(:,1)));
-    end
-end
-v2idx = logical(v1idx);
-v1idx = nonzeros(v1idx);
-
-groups = unique(grp)';
-[groups, sortidx] = sort(groups,'descend');
+groups = [0 1 5];
 groupnames = {
     'VS'
     'MCS'
-    'Healthy'
+    'CTRL'
     };
-groupnames = groupnames(sortidx);
 
 bands = {
     'Delta'
@@ -77,28 +66,30 @@ bands = {
     'Gamma'
     };
 
-trange = [0.5 0.1];
-% trange = (tvals <= trange(1) & tvals >= trange(2));
+trange = [0.9 0.1];
+trange = (tvals <= trange(1) & tvals >= trange(2));
 
-% m = find(strcmpi(measure,graph(:,1)));
+m = find(strcmpi(measure,graph(:,1)));
 
 barvals = zeros(3,length(groups));
 errvals = zeros(3,length(groups));
 
+selpatidx = ismember(grp,groups);
+
 if strcmp(measure,'modules')
-    groupvals = squeeze(mean(max(graph{m,weiorbin}(~v2idx,bandidx,trange,:),[],4),3));
+    groupvals = squeeze(mean(max(graph{m,weiorbin}(:,bandidx,trange,:),[],4),3));
     patvals = squeeze(mean(max(graphdata{m,weiorbin}(bandidx,:,:),[],3),2));
 elseif strcmp(measure,'mutual information')
-    groupvals = squeeze(mean(mean(graph{m,weiorbin}(~v2idx,grp == groups(g) & ~v2idx,bandidx,trange),4),2));
-    patvals = squeeze(mean(mean(graphdata{m,weiorbin}(grp == groups(g) & ~v2idx,bandidx,trange),4),2));
+    groupvals = squeeze(mean(mean(graph{m,weiorbin}(:,grp == groups(g),bandidx,trange),4),2));
+    patvals = squeeze(mean(mean(graphdata{m,weiorbin}(grp == groups(g),bandidx,trange),4),2));
 elseif strcmp(measure,'participation coefficient')
-    groupvals = squeeze(mean(std(graph{m,weiorbin}(~v2idx,bandidx,trange,:),[],4),3));
+    groupvals = squeeze(mean(std(graph{m,weiorbin}(selpatidx,bandidx,trange,:),[],4),3));
     patvals = squeeze(mean(std(graphdata{m,weiorbin}(bandidx,trange,:),[],3),2));
 elseif strcmp(measure,'median')
     groupvals = median(allcoh(:,bandidx,:),3);
     patvals = median(matrix(bandidx,:),2);
 else
-    groupvals = squeeze(mean(mean(graph{m,weiorbin}(~v2idx,bandidx,trange,:),4),3));
+    groupvals = squeeze(mean(mean(graph{m,weiorbin}(:,bandidx,trange,:),4),3));
     patvals = squeeze(mean(mean(graphdata{m,weiorbin}(bandidx,trange,:),3),2));
 end
 
@@ -106,7 +97,7 @@ plotvals = cat(1,groupvals,patvals);
 groupnames = cat(1,groupnames,{'Patient'});
 
 figure('Color','white','Name',basename)
-boxplot(plotvals,[grp(~v2idx); max(grp(~v2idx))+1],'labels',groupnames,'grouporder',{'2','1','0','3'},'symbol','r');
+boxplot(plotvals,[grp(selpatidx); max(grp(selpatidx))+1],'labels',groupnames,'symbol','r');
 
 if strcmp(param.plotticks,'on')
     set(gca,'FontName',fontname,'FontSize',fontsize);
