0647e5be420120699b43c832d2fa41f98687a756
Srivas Chennu <srivas@gmail.com>
2017-03-21 00:00:01 +0000

    Checked in Tue Mar 21 00:00:01 GMT 2017
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Liege-RestingState@92 1986c888-7f2f-4208-b3bb-bcc9d8df033b

---
+++ rejcount.m
@@ -0,0 +1,15 @@
+function rejdata = rejcount(listname)
+
+loadsubj
+loadpaths
+
+subjlist = eval(listname);
+
+rejdata = subjlist(:,[1 3]);
+for s = 1:size(subjlist,1)
+    basename = subjlist{s,1};
+    EEG = pop_loadset('filepath',filepath,'filename',[basename '.set'],'loadmode','info');
+    EEG.rejepoch = EEG.rejepoch(EEG.rejepoch < 60);
+    rejdata{s,3} = ((60 + length(EEG.rejepoch)) * 10)/60;
+    rejdata{s,4} = length(EEG.rejchan);
+end
